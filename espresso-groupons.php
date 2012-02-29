6910abf40926d9d2c9b054f4020f784f7ebad642
Darren Ethier <darren@eventespresso.com>
2012-02-29 02:40:54 +0000

    [touch: 780] - beginning work on the upgrade handling (for transferring existing groupon_codes to events_meta table)

--- espresso-groupons.php
+++ espresso-groupons.php
@@ -25,31 +25,53 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
+
+define("EVENTS_GROUPON_CODES_TABLE", $wpdb->prefix .'events_groupon_codes'); //Define Groupon db table shortname
 require_once("event_management_functions.php");
 require_once("use_groupon_code.php");
 require_once("groupons_admin_page.php");
+
+register_activation_hook(__FILE__,'event_espresso_groupon_install');//Install groupon tables
+register_deactivation_hook(__FILE__,'event_espresso_groupon_deactivate');
+add_action('plugins_loaded', 'event_espresso_update_banner');
+
 function event_espresso_groupon_install(){
-//Groupon database install
-$table_name = "events_groupon_codes";
-$table_version = "1.6";
-$sql = "id int(11) NOT NULL AUTO_INCREMENT,
-	groupon_code varchar(50) DEFAULT '0',
-	groupon_status INT DEFAULT '1',
-	groupon_holder TEXT DEFAULT NULL,
-	attendee_id int(11) DEFAULT '0',
-	date varchar(50) DEFAULT '0000-00-00',
-	PRIMARY KEY (id)";
-event_espresso_run_install ($table_name, $table_version, $sql);
-//Groupon database install end
-add_option('events_groupons_active', 'true', '', 'yes');
-update_option('events_groupons_active', 'true');
+	//get previous version
+	$prev_version = get_option('events_groupon_codes_table_version');
+	update_option('events_groupon_codes_table_version', '1.6');
+	//Groupon database install end
+	add_option('events_groupons_active', 'true', '', 'yes');
+	update_option('events_groupons_active', 'true');
+
+	//trigger import if pre 1.6 is installed.
+	if ( $prev_version <= 1.5 ) {
+		update_option('events_groupons_update', 'update');
+	}
+
 }
+
 function event_espresso_groupon_deactivate(){
 	update_option( 'events_groupons_active', 'false');
 }
-register_activation_hook(__FILE__,'event_espresso_groupon_install');//Install groupon tables
-register_deactivation_hook(__FILE__,'event_espresso_groupon_deactivate');
 
-//$event_espresso_groupon_dir = EVENT_ESPRESSO_UPLOAD_DIR."groupons/";
-//define("EVENT_ESPRESSO_GROUPON_DIR", $event_espresso_groupon_dir);
-define("EVENTS_GROUPON_CODES_TABLE", $wpdb->prefix .'events_groupon_codes'); //Define Groupon db table shortname
+function event_espresso_update_banner() {
+	if ( 'update' == get_option('events_groupons_update', 'update') ) {
+		//let's show the import banner and button for triggering the import.
+		add_action('wp_ajax_run_events_groupon_update', 'run_events_groupon_update' ); //ajax handler for when the upgrade button is pressed.
+		add_action('admin_notices', 'display_events_groupon_update_notice');
+	}
+}
+
+/**
+ * Do the upgrade to transfer groupon codes from the old table structure into the new one. TODO we need to work out how users will indicate which events the groupon_codes will be assigned to. And it would be good to allow batch assigning.
+ */
+function run_events_groupon_update() {
+	//todo
+}
+
+/**
+ * Display the upgrade container for handling the transfer of groupon codes from the old table structure into the new one.
+ */
+function display_events_groupon_update_notice() {
+	//todo
+}
