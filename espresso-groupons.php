84109516da234c407846872231299f2c59bdc047
Sidney Harrel <sidney@eventespresso.com>
2012-03-10 16:15:43 +0000

    fixing admin screens / menus [touch:813]

--- espresso-groupons.php
+++ espresso-groupons.php
@@ -92,7 +92,12 @@ function espresso_event_editor_groupon_meta_box($event) {
 }
 
 function espresso_register_groupon_event_editor_meta_boxes() {
+	global $espresso_premium;
 	add_meta_box('espresso_event_editor_groupon_box', __('Groupon Options', 'event_espresso'), 'espresso_event_editor_groupon_meta_box', 'toplevel_page_events', 'side', 'default');
+	add_meta_box('espresso_news_post_box', __('New @ Event Espresso', 'event_espresso'), 'espresso_news_post_box', 'event-espresso_page_groupons', 'side');
+	add_meta_box('espresso_links_post_box', __('Helpful Plugin Links', 'event_espresso'), 'espresso_links_post_box', 'event-espresso_page_groupons', 'side');
+	if (!$espresso_premium)
+		add_meta_box('espresso_sponsors_post_box', __('Sponsors', 'event_espresso'), 'espresso_sponsors_post_box', 'event-espresso_page_groupons', 'side');
 }
 
 add_action('current_screen', 'espresso_register_groupon_event_editor_meta_boxes');
