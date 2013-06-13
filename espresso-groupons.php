8b033ba16fb5085a5322ef43a9c44dccb34e0ed4
Brent Christensen <brent@eventespresso.com>
2013-06-13 10:46:08 -0700

    fixes missing $wpdb [touch:2937]

--- espresso-groupons.php
+++ espresso-groupons.php
@@ -77,4 +77,5 @@ register_deactivation_hook(__FILE__,'event_espresso_groupon_deactivate');
 
 //$event_espresso_groupon_dir = EVENT_ESPRESSO_UPLOAD_DIR."groupons/";
 //define("EVENT_ESPRESSO_GROUPON_DIR", $event_espresso_groupon_dir);
+global $wpdb;
 define("EVENTS_GROUPON_CODES_TABLE",$wpdb->prefix."events_groupon_codes"); //Define Groupon db table shortname
